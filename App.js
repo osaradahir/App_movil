905d64de70a1997bad7d961b7783e81699aceb67
Oscar_AGS <122835599+osaradahir@users.noreply.github.com>
2024-04-18 15:18:52 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,19 +6,22 @@ import MenuScreen from './MenuScreen';
 import DisponibilidadScreen from './DisponibilidadScreen';
 import HorariosScreen from './HorariosScreen';
 import AulasScreen from './AulasScreen';
+import LoginGrupoScreen from './LoginGrupoScreen';
+import HorarioGrupoScreen from './HorarioGrupoScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="MenuScreen" component={MenuScreen} />
         <Stack.Screen name="DisponibilidadScreen" component={DisponibilidadScreen} />
         <Stack.Screen name="HorariosScreen" component={HorariosScreen} />
         <Stack.Screen name="AulasScreen" component={AulasScreen} />
-        
+        <Stack.Screen name="LoginGrupoScreen" component={LoginGrupoScreen} />
+        <Stack.Screen name="HorarioGrupoScreen" component={HorarioGrupoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
